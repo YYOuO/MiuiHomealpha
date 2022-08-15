8a129d458eb6f280bca78c9fc5bece3bd7d853ab
YYOuO <eddy101173@gmail.com>
2022-08-15 22:15:21 +0800

    customize.sh create

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 MiuiHomealpha
 ===
-修改桌面作者：[TAT趙](https://space.bilibili.com/324056528/video)
+## 修改桌面作者：[TAT趙](https://space.bilibili.com/324056528/video)
 ---
 Introduction
 ---
 This is a magisk module to install unoffical miuihome on your xiaomi device. 
+There are two version now.
+堆疊版 & 修改版
 
 Requirement
 ---
@@ -14,7 +16,9 @@ Requirement
 
 Install
 ---
-1. Install Magisk(you can go to this [website](http://lollol.infinityfreeapp.com) you learn)
+**SUGGEST YOU TO INSTALL BOOTLOOP PROTECTOR FIRST**
+**OR YOU WANT TO GET A BOOTLOOP IS FINE :D**
+1. Install Magisk(you can go to this [website](http://lollol.infinityfreeapp.com) to learn)
 2. Flash LSposed module
 3. Flash this module
 4. Reboot
---
+++ customize.sh
@@ -0,0 +1,35 @@
+# 模組名稱
+name="`grep_prop name $TMPDIR/module.prop`"
+
+# 模組id
+id="`grep_prop id $TMPDIR/module.prop`"
+
+# 模組作者
+author="`grep_prop author $TMPDIR/module.prop`"
+
+# 模組版本
+version="`grep_prop version $TMPDIR/module.prop`"
+
+# 裝置信息
+device="`getprop ro.product.device`"
+
+# Android版本
+Android_version="`grep_prop ro.build.version.release`"
+
+# Miui版本
+MIUI_version="`grep_prop ro.miui.ui.version.name`"
+
+# 介绍
+ui_print "*******************************************"
+ui_print "   $name "
+ui_print "   Author $author"
+ui_print "   Version: $version"
+sleep 2
+ui_print "*******************************************"
+ui_print "   設備信息"
+ui_print "   機型: $device"
+ui_print "   Android 版本: $Android_version"
+ui_print "   MIUI版本: $MIUI_version"
+ui_print "*******************************************"
+sleep 2
+ui_print " 我還不會判斷版本喔QQ- 提醒：如果遇到bootloop 或者卡米adb wait-for-device shell magisk --remove-modules好用的"
